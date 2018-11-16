4759b74812f3de8d886db93d933252ed886d58f5
Nir Izraeli <nirizr@gmail.com>
2018-11-16 14:26:10 -0800

    wip

--- idaplugin/rematch/collectors/annotations/prototype.py
+++ idaplugin/rematch/collectors/annotations/prototype.py
@@ -9,6 +9,14 @@ class PrototypeAnnotation(annotation.Annotation):
   type = 'prototype'
 
   def data(self):
+    # get function type info
+    ti = ida_typeinf.tinfo_t()
+    ida_nalt.get_tinfo(ti, self.offset)
+
+    # skip it if it's empty
+    if ti.empty():
+      return None
+
     t = ida_typeinf.idc_get_type(self.offset)
     # if failed getting type, there's no annotation here
     if t is None:
